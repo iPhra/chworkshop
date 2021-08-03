19c10469817ba71f45eef3838532219c5996ff0b
lorefr <lorefr@amazon.com>
2021-08-03 17:00:12 +0200

    Push training script

--- train.py
+++ train.py
@@ -236,7 +236,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--log-interval",
         type=int,
-        default=100,
+        default=64,
         metavar="N",
         help="how many batches to wait before logging training status",
     )
